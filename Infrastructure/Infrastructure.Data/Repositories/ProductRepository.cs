cf4d31bf017ca1bd8d8b70589bc8de8137628a50
Filipe Ferreira de Souza <filipeffs10@gmail.com>
2018-06-18 19:49:38 -0300

    saving work

---
+++ Infrastructure/Infrastructure.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models.Entities;
+using Domain.Interfaces.Repository;
+using Domain.Interfaces;
+using System.Linq;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+
+        public ProductRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+
+        public override IQueryable<Product> Get()
+        {
+            return UnitOfWork.Db<Product>();
+        }
+    }
+}
---
+++ Infrastructure/Infrastructure.Data/Repositories/Repository.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Domain.Interfaces;
+using Domain.Interfaces.Repository;
+
+namespace Infrastructure.Data.Repositories
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+
+        #region Properties
+
+        public IUnitOfWork UnitOfWork { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        protected Repository(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public abstract IQueryable<T> Get();
+
+        #endregion
+    }
+}
